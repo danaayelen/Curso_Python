c3918c59f6f661206f5aa413cf1b024c5f9ad3cb
Dana Ayelen Bazán Domínguez <132296912+danaayelen@users.noreply.github.com>
2025-10-23 12:55:55 -0600

    Se orgnizo por carpeta el curso

---
+++ Seccion1/OperadoresBits.py
@@ -0,0 +1,20 @@
+#Operadores de identidad 
+x=["manzana","pera"]
+y=["manzana","pera"]
+z=x
+print("1. x es z")
+print(x is z)
+print("2. x es y")
+#is apunta al objeto en memoria, no compara 
+print(x is y)
+print("3. x es igual y")
+print(x==y)
+
+#operadores de pertenencia
+print("fresa se encunentra en x?")
+print("fresa" in x)
+
+print("manzana se encunentra en x?")
+print("manzana" in x)
+
+
---
+++ Seccion1/variablesAgregadas.py
@@ -0,0 +1,19 @@
+#List: es una coleccion que esta ordenada y cambiable.Permite duplicar miembros
+miList=["Manzana", "pera", "uvas"]
+print(miList)
+
+#Tuple: es una coleccion que esta ordenada e inmutable.Permite duplicar miembros
+miTupla=["Manzana", "pera", "uvas", "uvas"]
+print(miTupla)
+
+
+miSet=["Manzana", "pera", "uvas", "uvas"]
+print(miSet)
+
+#diccionario: es una coleccion que no esta ordenada, modificable e indexada
+miDiccionario={
+    "rojo":"manzana",
+    "verde":"pera",
+    "morado":"uva"
+}
+print(miDiccionario)
